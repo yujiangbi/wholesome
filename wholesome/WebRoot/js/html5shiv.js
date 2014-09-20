27ddb7add1e0cc8b81ed62c162e38d55f0baaf97
yujb <yujb@yujb-PC>
2014-09-20 10:49:47 +0800

    添加项目

---
+++ wholesome/WebRoot/js/html5shiv.js
@@ -0,0 +1,301 @@
+<!DOCTYPE html> 
+<html class="no-js" lang="en-US" prefix="og: http://ogp.me/ns#">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Page not found - Download Ebook</title>
+
+	<link rel="pingback" href="http://bismillahlab.web.id/xmlrpc.php">
+	
+	<script>document.documentElement.className = document.documentElement.className.replace("no-js","js");</script>
+
+<!-- This site is optimized with the Yoast WordPress SEO plugin v1.5.3.3 - https://yoast.com/wordpress/plugins/seo/ -->
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="object" />
+<meta property="og:title" content="Page not found - Download Ebook" />
+<meta property="og:site_name" content="Download Ebook" />
+<!-- / Yoast WordPress SEO plugin. -->
+
+<link rel="alternate" type="application/rss+xml" title="Download Ebook &raquo; Feed" href="http://bismillahlab.web.id/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Download Ebook &raquo; Comments Feed" href="http://bismillahlab.web.id/comments/feed/" />
+<link rel='stylesheet' id='jetpack-subscriptions-css'  href='http://bismillahlab.web.id/wp-content/plugins/jetpack/modules/subscriptions/subscriptions.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='bootstrap-css'  href='http://bismillahlab.web.id/wp-content/plugins/easy-bootstrap-shortcodes/styles/bootstrap.min.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='bootstrap-fa-icon-css'  href='http://bismillahlab.web.id/wp-content/plugins/easy-bootstrap-shortcodes/styles/font-awesome.min.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='contact-form-7-css'  href='http://bismillahlab.web.id/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='fancybox-style-css'  href='http://bismillahlab.web.id/wp-content/plugins/light/css/jquery.fancybox.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='jetpack-widgets-css'  href='http://bismillahlab.web.id/wp-content/plugins/jetpack/modules/widgets/widgets.css?ver=20121003' type='text/css' media='all' />
+<link rel='stylesheet' id='style-css'  href='http://bismillahlab.web.id/wp-content/themes/hueman/style.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='responsive-css'  href='http://bismillahlab.web.id/wp-content/themes/hueman/responsive.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='font-awesome-css'  href='http://bismillahlab.web.id/wp-content/themes/hueman/fonts/font-awesome.min.css?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='ebs_dynamic_css-css'  href='http://bismillahlab.web.id/wp-content/plugins/easy-bootstrap-shortcodes/styles/ebs_dynamic_css.php?ver=3.9.1' type='text/css' media='all' />
+<link rel='stylesheet' id='sharedaddy-css'  href='http://bismillahlab.web.id/wp-content/plugins/jetpack/modules/sharedaddy/sharing.css?ver=3.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='http://bismillahlab.web.id/wp-content/plugins/jetpack/_inc/genericons/genericons.css?ver=3.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='widget-grid-and-list-css'  href='http://bismillahlab.web.id/wp-content/plugins/jetpack/modules/widgets/widget-grid-and-list.css?ver=3.9.1' type='text/css' media='all' />
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-includes/js/jquery/jquery.js?ver=1.11.0'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/plugins/easy-bootstrap-shortcodes/js/bootstrap.min.js?ver=3.9.1'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/themes/hueman/js/jquery.flexslider.min.js?ver=3.9.1'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://bismillahlab.web.id/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://bismillahlab.web.id/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 3.9.1" />
+<!--[if lt IE 9]>
+<script src="http://bismillahlab.web.id/wp-content/themes/hueman/js/ie/html5.js"></script>
+<script src="http://bismillahlab.web.id/wp-content/themes/hueman/js/ie/selectivizr.js"></script>
+<![endif]-->
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+<style type="text/css">
+/* Dynamic CSS: For no styles in head, copy and put the css below in your child theme's style.css, disable dynamic styles */
+</style>
+</head>
+
+<body class="error404 col-3cm full-width topbar-enabled ie8 ie">
+
+<div id="wrapper">
+
+	<header id="header">
+	
+					<nav class="nav-container group" id="nav-topbar">
+				<div class="nav-toggle"><i class="fa fa-bars"></i></div>
+				<div class="nav-text"><!-- put your mobile menu text here --></div>
+				<div class="nav-wrap container"><ul id="menu-menu-atas" class="nav container-inner group"><li id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-7"><a href="http://bismillahlab.web.id/">Home</a></li>
+</ul></div>
+				
+				<div class="container">
+					<div class="container-inner">		
+						<div class="toggle-search"><i class="fa fa-search"></i></div>
+						<div class="search-expand">
+							<div class="search-expand-inner">
+								<form method="get" class="searchform themeform" action="http://bismillahlab.web.id/">
+	<div>
+		<input type="text" class="search" name="s" onblur="if(this.value=='')this.value='To search type and hit enter';" onfocus="if(this.value=='To search type and hit enter')this.value='';" value="To search type and hit enter" />
+	</div>
+</form>							</div>
+						</div>
+					</div><!--/.container-inner-->
+				</div><!--/.container-->
+				
+			</nav><!--/#nav-topbar-->
+				
+		<div class="container group">
+			<div class="container-inner">
+				
+				<div class="group pad">
+					<p class="site-title"><a href="http://bismillahlab.web.id/" rel="home">Download Ebook</a></p>
+					<p class="site-description">Ebook Programming, Internet, Education, Academi</p>				</div>
+				
+									<nav class="nav-container group" id="nav-header">
+						<div class="nav-toggle"><i class="fa fa-bars"></i></div>
+						<div class="nav-text"><!-- put your mobile menu text here --></div>
+						<div class="nav-wrap container"><ul id="menu-menu-bismillah" class="nav container-inner group"><li id="menu-item-8" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-8"><a href="http://bismillahlab.web.id/category/academic/">Academic</a></li>
+<li id="menu-item-9" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-9"><a href="http://bismillahlab.web.id/category/agile/">Agile</a></li>
+<li id="menu-item-139" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-139"><a href="http://bismillahlab.web.id/category/children/">Children</a></li>
+<li id="menu-item-10" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-10"><a href="http://bismillahlab.web.id/category/cookbooks/">CookBooks</a></li>
+<li id="menu-item-27" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-27"><a href="http://bismillahlab.web.id/category/free-ebook/">Free Ebook</a></li>
+<li id="menu-item-11" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-11"><a href="http://bismillahlab.web.id/category/internet/">Internet</a></li>
+<li id="menu-item-138" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-138"><a href="http://bismillahlab.web.id/category/programming/">Programming</a></li>
+</ul></div>
+					</nav><!--/#nav-header-->
+								
+			</div><!--/.container-inner-->
+		</div><!--/.container-->
+		
+	</header><!--/#header-->
+	
+	<div class="container" id="page">
+		<div class="container-inner">
+			<div class="main">
+				<div class="main-inner group">
+<section class="content">
+
+	<div class="page-title pad group">
+
+			<h1><i class="fa fa-exclamation-circle"></i>Error 404. <span>Page not found!</span></h1>
+		
+	
+</div><!--/.page-title-->	
+	<div class="pad group">		
+		
+		<div class="notebox">
+			<form method="get" class="searchform themeform" action="http://bismillahlab.web.id/">
+	<div>
+		<input type="text" class="search" name="s" onblur="if(this.value=='')this.value='To search type and hit enter';" onfocus="if(this.value=='To search type and hit enter')this.value='';" value="To search type and hit enter" />
+	</div>
+</form>		</div>
+		
+		<div class="entry">
+			<p>The page you are trying to reach does not exist, or has been moved. Please use the menus or the search box to find what you are looking for.</p>
+		</div>
+		
+	</div><!--/.pad-->
+	
+</section><!--/.content-->
+
+
+	<div class="sidebar s1">
+		
+		<a class="sidebar-toggle" title="Expand Sidebar"><i class="fa icon-sidebar-toggle"></i></a>
+		
+		<div class="sidebar-content">
+			
+			<div class="sidebar-top group">
+				<p>Follow:</p>
+							</div>
+			
+						
+						
+			<div id="search-2" class="widget widget_search"><form method="get" class="searchform themeform" action="http://bismillahlab.web.id/">
+	<div>
+		<input type="text" class="search" name="s" onblur="if(this.value=='')this.value='To search type and hit enter';" onfocus="if(this.value=='To search type and hit enter')this.value='';" value="To search type and hit enter" />
+	</div>
+</form></div>		<div id="recent-posts-2" class="widget widget_recent_entries">		<h3>Recent Posts</h3>		<ul>
+					<li>
+				<a href="http://bismillahlab.web.id/2014/07/download-ebook-programmeren-voor-kinderen/">Download Ebook Programmeren voor kinderen</a>
+						</li>
+					<li>
+				<a href="http://bismillahlab.web.id/2014/07/download-ebook-heron-tortoise/">Download Ebook The Heron and the Tortoise</a>
+						</li>
+					<li>
+				<a href="http://bismillahlab.web.id/2014/07/download-ebook-sally-strange/">Download Ebook Sally Strange</a>
+						</li>
+					<li>
+				<a href="http://bismillahlab.web.id/2014/07/download-ebook-modernizing-legacy-applications-php/">Download Ebook Modernizing Legacy Applications In PHP</a>
+						</li>
+					<li>
+				<a href="http://bismillahlab.web.id/2014/07/download-ebook-working-effectively-unit-tests/">Download Ebook Working Effectively with Unit Tests</a>
+						</li>
+				</ul>
+		</div><div id="recent-comments-2" class="widget widget_recent_comments"><h3>Recent Comments</h3><ul id="recentcomments"><li class="recentcomments"><a href='https://wordpress.org/' rel='external nofollow' class='url'>Mr WordPress</a> on <a href="http://bismillahlab.web.id/2014/07/hello-world/#comment-1">Hello world!</a></li></ul></div><div id="archives-2" class="widget widget_archive"><h3>Archives</h3>		<ul>
+	<li><a href='http://bismillahlab.web.id/2014/07/'>July 2014</a></li>
+		</ul>
+</div><div id="categories-2" class="widget widget_categories"><h3>Categories</h3>		<ul>
+	<li class="cat-item cat-item-4"><a href="http://bismillahlab.web.id/category/academic/" title="academic ebook">Academic</a>
+</li>
+	<li class="cat-item cat-item-15"><a href="http://bismillahlab.web.id/category/children/" title="View all posts filed under Children">Children</a>
+</li>
+	<li class="cat-item cat-item-6"><a href="http://bismillahlab.web.id/category/cookbooks/" title="CookBooks Ebook">CookBooks</a>
+</li>
+	<li class="cat-item cat-item-11"><a href="http://bismillahlab.web.id/category/free-ebook/" title="View all posts filed under Free Ebook">Free Ebook</a>
+</li>
+	<li class="cat-item cat-item-7"><a href="http://bismillahlab.web.id/category/internet/" title="Internet Ebook">Internet</a>
+</li>
+	<li class="cat-item cat-item-14"><a href="http://bismillahlab.web.id/category/programming/" title="View all posts filed under Programming">Programming</a>
+</li>
+	<li class="cat-item cat-item-1"><a href="http://bismillahlab.web.id/category/uncategorized/" title="View all posts filed under Uncategorized">Uncategorized</a>
+</li>
+		</ul>
+</div>			
+		</div><!--/.sidebar-content-->
+		
+	</div><!--/.sidebar-->
+
+	
+<div class="sidebar s2">
+	
+	<a class="sidebar-toggle" title="Expand Sidebar"><i class="fa icon-sidebar-toggle"></i></a>
+	
+	<div class="sidebar-content">
+		
+		<div class="sidebar-top group">
+			<p>More</p>
+		</div>
+		
+				
+		<div id="blog_subscription-2" class="widget jetpack_subscription_widget"><h3><label for="subscribe-field">Subscribe to Blog via Email</label></h3>
+		<form action="#" method="post" accept-charset="utf-8" id="subscribe-blog-blog_subscription-2">
+			<p id="subscribe-text">Enter your email address to subscribe to this blog and receive notifications of new posts by email.</p>
+			<p id="subscribe-email"><input type="text" name="email" value="Email Address" id="subscribe-field" onclick="if ( this.value == 'Email Address' ) { this.value = ''; }" onblur="if ( this.value == '' ) { this.value = 'Email Address'; }" /></p>
+
+			<p id="subscribe-submit">
+				<input type="hidden" name="action" value="subscribe" />
+				<input type="hidden" name="source" value="http://bismillahlab.web.id/themes/srikandi/js/html5shiv.js" />
+				<input type="hidden" name="sub-type" value="widget" />
+				<input type="hidden" name="redirect_fragment" value="blog_subscription-2" />
+								<input type="submit" value="Subscribe" name="jetpack_subscriptions_widget" />
+			</p>
+		</form>
+
+		
+</div><div id="rss_links-2" class="widget widget_rss_links"><h3>RSS</h3><ul><li><a href="http://bismillahlab.web.id/feed/" title="Subscribe to Posts">RSS - Posts</a></li><li><a href="http://bismillahlab.web.id/comments/feed/" title="Subscribe to Comments">RSS - Comments</a></li></ul>
+</div><div id="facebook-likebox-2" class="widget widget_facebook_likebox"><h3><a href="https://www.facebook.com/PortfolioPremiumThemes">Like Facebook Fans Page</a></h3><iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FPortfolioPremiumThemes&#038;width=200&#038;height=432&#038;colorscheme=light&#038;show_faces=true&#038;stream=false&#038;show_border=true&#038;header=false&#038;force_wall=false" scrolling="no" frameborder="0" style="border: none; overflow: hidden; width: 200px;  height: 432px; background: #fff"></iframe></div><div id="top-posts-2" class="widget widget_top-posts"><h3>Popular Post</h3><ul><li><a href="http://bismillahlab.web.id/2014/07/download-ebook-laravel-4-official-documentation/" class="bump-view" data-bump-view="tp">Download Ebook Laravel 4 Official Documentation</a></li>
+<li><a href="http://bismillahlab.web.id/2014/07/download-ebook-implementing-laravel/" class="bump-view" data-bump-view="tp">Download Ebook Implementing Laravel</a></li>
+</ul></div><div id="text-2" class="widget widget_text">			<div class="textwidget"><a href="http://info.flagcounter.com/Mn8a"><img src="http://s11.flagcounter.com/count/Mn8a/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_50/viewers_0/labels_0/pageviews_0/flags_0/" alt="Flag Counter" border="0"></a></div>
+		</div>		
+	</div><!--/.sidebar-content-->
+	
+</div><!--/.sidebar-->	
+
+				</div><!--/.main-inner-->
+			</div><!--/.main-->
+		</div><!--/.container-inner-->
+	</div><!--/.container-->
+
+	<footer id="footer">	
+				
+				
+		<section class="container" id="footer-bottom">
+			<div class="container-inner">
+				
+				<a id="back-to-top" href="#"><i class="fa fa-angle-up"></i></a>
+				
+				<div class="pad group">
+					
+					<div class="grid one-half">
+						
+												
+						<div id="copyright">
+															<p>Download Ebook &copy; 2014. All Rights Reserved.</p>
+													</div><!--/#copyright-->
+						
+												<div id="credit">
+							<p>Powered by <a href="http://wordpress.org" rel="nofollow">WordPress</a>. Theme by <a href="http://alxmedia.se" rel="nofollow">Alx</a>.</p>
+						</div><!--/#credit-->
+												
+					</div><!--/.pad-->
+					
+					<div class="grid one-half last">	
+											</div>
+				
+				</div>
+				
+			</div><!--/.container-inner-->
+		</section><!--/.container-->
+		
+	</footer><!--/#footer-->
+
+</div><!--/#wrapper-->
+
+	<div style="display:none">
+	</div>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js?ver=3.50.0-2014.02.05'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpcf7 = {"loaderUrl":"http:\/\/bismillahlab.web.id\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ..."};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=3.8.1'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/plugins/light/js/jquery.fancybox.pack.js?ver=3.9.1'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/plugins/light/js/light.js?ver=3.9.1'></script>
+<script type='text/javascript' src='http://s0.wp.com/wp-content/js/devicepx-jetpack.js?ver=201435'></script>
+<script type='text/javascript' src='http://s.gravatar.com/js/gprofiles.js?ver=2014Augaa'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var WPGroHo = {"my_hash":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/plugins/jetpack/modules/wpgroho.js?ver=3.9.1'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/themes/hueman/js/jquery.jplayer.min.js?ver=3.9.1'></script>
+<script type='text/javascript' src='http://bismillahlab.web.id/wp-content/themes/hueman/js/scripts.js?ver=3.9.1'></script>
+<!--[if lt IE 9]>
+<script src="http://bismillahlab.web.id/wp-content/themes/hueman/js/ie/respond.js"></script>
+<![endif]-->
+
+	<script src="http://stats.wordpress.com/e-201435.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	st_go({v:'ext',j:'1:3.0.2',blog:'70732272',post:'0',tz:'0'});
+	var load_cmc = function(){linktracker_init(70732272,0,2);};
+	if ( typeof addLoadEvent != 'undefined' ) addLoadEvent(load_cmc);
+	else load_cmc();
+	</script></body>
+</html>
